dd3d1c608b5539c5fb4827d0244353cb480227a0
Luca Critelli <lucacri@gmail.com>
2018-03-12 12:16:53 -0400

    Wrong path

--- rootfs/etc/cont-init.d/10-set-service-account.sh
+++ rootfs/etc/cont-init.d/10-set-service-account.sh
@@ -3,5 +3,5 @@
 if [ -n "$SERVICE_ACCOUNT_JSON_PATH" ]; then
    echo "Enabling Service account $SERVICE_ACCOUNT_JSON_PATH";
 
-    gcloud auth activate-service-account --key-file $SERVICE_ACCOUNT_JSON_PATH
+    /google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $SERVICE_ACCOUNT_JSON_PATH
 fi
